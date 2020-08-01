151f5bcc51805f600f4ec7332dc57eca53b64c0c
Alexey Lebedeff <binarin@binarin.ru>
2020-08-01 21:58:01 +0200

    Day 1

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "aoc-2018"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,35 @@
+import scala.io.Source
+import scala.collection.mutable.Set
+object Main extends App {
+
+  val deltas = Source.fromFile("data/01-1.txt").getLines().map (_.toInt).to(List)
+
+  println(deltas.sum)
+
+  var repeated = LazyList.continually(deltas.to(LazyList)).flatten
+
+  val seen = Set[Int]()
+
+  var freq = 0
+
+  var done = false
+
+  var count = 0
+
+  while (!done) {
+    count += 1
+    val cur = repeated.head
+    repeated = repeated.tail
+
+    freq += cur
+    if (seen(freq)) {
+      println(freq)
+      done = true;
+    }
+
+    seen.add(freq)
+  }
+
+  println(count)
+
+}
